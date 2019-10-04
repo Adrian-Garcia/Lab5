d04df00c129df7104d0b6be2d53f08af7c2f8350
Adrian Garcia <adriangarlop@hotmail.com>
2019-10-03 22:13:15 -0500

    finished Lab5

--- indx.js
+++ indx.js
@@ -1,8 +1,33 @@
-console.log(1);
+let count = 0;
+
 $("button").on('click', function(e) {
+		
+	e.preventDefault();
+
+	if ($('#data').val() !=  "") {
+
+		$(".list").append(`<div class="bullet">
+								<div> ${$('#data').val()}</div>
+								<button class="check"> Check </button>
+								<button class="erase"> Erase </button>
+							</div>`);
+	}
+
+	$('#data').val('');
+});
+
+$(".list").on("click", ".bullet>.check", function(e) {
 
 	e.preventDefault();
 
-	
-
+	if ($(e.target).parent().find("div").attr("style")) 
+		$(e.target).parent().find("div").attr("style", "");
+	else
+		$(e.target).parent().find("div").attr("style", "text-decoration: line-through;");
 });
+
+$(".list").on("click", ".bullet>.erase", function(e) {
+	e.preventDefault();
+	$(e.target).parent().remove();
+});
+
